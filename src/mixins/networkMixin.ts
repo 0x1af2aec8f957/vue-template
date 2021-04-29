61217baa758bcfff5ff1224e7d8236dce0d0dd70
23fd5097H <0x23fd5097@gmail.com>
2021-04-29 14:04:21 +0800

    style: 优化 networkMixin 代码

--- src/mixins/networkMixin.ts
+++ src/mixins/networkMixin.ts
@@ -14,26 +14,6 @@ function getNetworkState(): NetworkState { // 获取网络状态
     return onLine ? NetworkState.ONE_LINE : NetworkState.OFF_LINE;
 }
 
-export function useNetwork(_props: any) { // setup[composition-api]，可在组件内部监听 networkState 获取网络状态
-    const networkState = ref<NetworkState>(getNetworkState()); // 初始化网络状态
-
-    function updateNetworkState(): void { // 变更 networkState
-        networkState.value = getNetworkState();
-    }
-
-    onMounted((): void => { // 监听
-        addEventListener(window, 'online', updateNetworkState);
-        addEventListener(window, 'offline', updateNetworkState);
-    });
-    onUnmounted(() => { // 移除
-        removeEventListener(window, 'online', updateNetworkState);
-        removeEventListener(window, 'offline', updateNetworkState);
-    });
-    return { // 这里返回的任何内容都可以用于组件的其余部分
-        networkState
-    };
-}
-
 export const networkMixin: ComponentOptions = {
     data(): {
         networkState: NetworkState
@@ -56,6 +36,26 @@ export const networkMixin: ComponentOptions = {
         removeEventListener(window, 'offline', this.updateNetworkState);
     }
 };
+
+export function useNetwork(_props: any) { // setup[composition-api]，可在组件内部监听 networkState 获取网络状态
+    const networkState = ref<NetworkState>(getNetworkState()); // 初始化网络状态
+
+    function updateNetworkState(): void { // 变更 networkState
+        networkState.value = getNetworkState();
+    }
+
+    onMounted((): void => { // 监听
+        addEventListener(window, 'online', networkMixin.methods.updateNetworkState);
+        addEventListener(window, 'offline', networkMixin.methods.updateNetworkState);
+    });
+    onUnmounted(() => { // 移除
+        removeEventListener(window, 'online', networkMixin.methods.updateNetworkState);
+        removeEventListener(window, 'offline', networkMixin.methods.updateNetworkState);
+    });
+    return { // 这里返回的任何内容都可以用于组件的其余部分
+        networkState
+    };
+}
 
 export default {
     install: (app: App): void => {

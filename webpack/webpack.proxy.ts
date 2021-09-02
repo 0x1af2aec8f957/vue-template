414683c219e1b8f6597c34aa639f010f04dfbdac
0x23fd5097 <0x23fd5097@gmail.com>
2021-09-02 15:13:45 +0800

    chore: 新增开发环境自动登录示例

--- webpack/webpack.proxy.ts
+++ webpack/webpack.proxy.ts
@@ -1,66 +1,57 @@
+import { responseInterceptor, Options, fixRequestBody } from 'http-proxy-middleware';
+import axios from 'axios';
+import type * as http from 'http';
+
 type _Cookie = false | string | {
     [domain: string]: string
 };
 
 export interface DevServerProxy{ // doc: https://github.com/chimurai/http-proxy-middleware#http-proxy-options
-    [path: string]: {
-        target: string;
-        pathRewrite?: {
-            [path: string]: string
-        } | ((path: string, req?: object) => string | Promise<string>);
-        router?: {
-            [path: string]: string
-        } | ((req: object) => string | Promise<string>);
-        forward?: string;
-        agent?: { // doc: https://nodejs.org/api/http.html#http_new_agent_options
-            keepAlive?: boolean,
-            keepAliveMsecs?: number,
-            maxSockets?: number,
-            maxTotalSockets?: number,
-            maxFreeSockets?: number,
-            scheduling?: string,
-            timeout?: number
-        };
-        ssl?: { // doc: https://nodejs.org/api/https.html#https_https_createserver_options_requestlistener
-            key: string,
-            cert: string,
-        };
-        logLevel?: 'debug' | 'info' | 'warn' | 'error' | 'silent',
-        logProvider?: (provider: any) => object,
-        ws?: boolean;
-        xfwd?: boolean;
-        secure?: boolean;
-        toProxy?: boolean;
-        prependPath?: boolean;
-        ignorePath?: boolean;
-        localAddress?: string;
-        changeOrigin?: boolean;
-        preserveHeaderKeyCase?: boolean;
-        auth?: string;
-        hostRewrite?: string | number;
-        autoRewrite?: string | number;
-        protocolRewrite?: 'http' | 'https';
-        cookieDomainRewrite?: _Cookie;
-        cookiePathRewrite?: _Cookie;
-        headers?: {
-            [requestFieldKey: string]: any // doc: https://en.wikipedia.org/wiki/List_of_HTTP_header_fields#Request_fields
-        };
-        proxyTimeout?: number; // in millis
-        timeout?: number; // in millis
-        followRedirects?: boolean;
-        selfHandleResponse?: boolean;
-        buffer?: any;
-    }
+    [path: string]: Options
 }
 
+const certificatePaylod = new Map(); // 代理时的授权访问凭证
 const proxy: DevServerProxy = {
     '/api': {
         // target: 'http://192.168.3.1:5001',
         target: 'https://www.example.com',
-        changeOrigin: true
+        changeOrigin: true,
         /* pathRewrite: {
             '^/': '', // rewrite...
         }, */
+        selfHandleResponse: true, // 开启拦截来自上游的响应 responseInterceptor
+        onProxyReq(proxyReq, req: http.IncomingMessage, res: http.ServerResponse) {
+            // add custom header to request
+            certificatePaylod.set('domain', `${proxyReq.protocol}//${proxyReq.host}`/* 代理配置中的 target 属性值 */); // 设置许可域
+            if (certificatePaylod.has('token')) proxyReq.setHeader('token', certificatePaylod.get('token')); // 如果 certificatePaylod 存在 token 则自动写入header
+            // or log the req
+        },
+        onProxyRes: responseInterceptor(async (responseBuffer: Buffer, proxyRes: http.IncomingMessage, req: http.IncomingMessage, res: http.ServerResponse) => {
+            const response = responseBuffer.toString('utf8'); // 从代理目标获取到的数据
+            const result = JSON.parse(response); // 数据源解析成JSON格式
+
+            if (result.respCode === '403') { // 登录凭证过期
+                await axios.post(`${certificatePaylod.get('domain')}/login`, { // 处理自动授权登录并保存访问凭证
+                    userName: 'example@examplecom',
+                    password: 'example'
+                }, {
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                }).then(({headers, data}) => {
+                    if (headers.token) certificatePaylod.set('token', headers.token); // 存储token
+                    if (data.id) certificatePaylod.set('userId', data.userId); // 存储userId
+                    console.log(`已自动完成授权，授权用户 -> ${certificatePaylod.get('userId')}`);
+                });
+
+                return JSON.stringify({ // manipulate response and return the result
+                    code: 'http-proxy-middleware-certificate-error',
+                    message: 'http-proxy-middleware-error: 登录失效，已自动重新登录，请刷新界面'
+                });
+            }
+
+            return response;
+        })
     }
 };
 

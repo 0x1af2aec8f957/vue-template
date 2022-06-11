6c92325bc9ace743396462f24589a17e4c7f43ff
23fd5097H <0x23fd5097@gmail.com>
2022-06-11 13:47:20 +0800

    Update routes.ts

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,10 @@
 import type { RouteRecordRaw } from 'vue-router';
 
+import { RouterView } from 'vue-router';
+import { h } from 'vue';
+
 const routes: RouteRecordRaw[] = [
-    {
+    { /// view mode
         path: '/',
         name: 'main',
         component: () => import(/* webpackChunkName: "main-view" */ './views/Main.vue'),
@@ -14,11 +17,23 @@ const routes: RouteRecordRaw[] = [
             },
             {
                 path: 'home',
-                name: 'home',
-                component: () => import(/* webpackChunkName: "home" */ './views/Home.vue'),
+                name: 'main-home',
+                component: () => import(/* webpackChunkName: "main-home" */ './views/Home.vue'),
                 meta: { title: '首页', keepAlive: false }
             }
         ]
+    },
+    { /// menu mode
+        path: 'menu',
+        name: 'menu',
+        component: { render: () => h(RouterView) }, // 嵌套路由无固定component模式
+        meta: { title: '菜单嵌套递归占位模式', keepAlive: false },
+        children: [{
+            path: 'home',
+            name: 'menu-home',
+            component: () => import(/* webpackChunkName: "menu-home" */ './views/Home.vue'),
+            meta: { title: '首页', keepAlive: false }
+        }]
     }
 ];
 

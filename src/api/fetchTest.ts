6ffa72233358789df2eee93e83dfc9e4f83a59d0
Abner <0x23fd5097@gmail.com>
2023-02-09 18:41:47 +0800

    fix: 修正模板代码

--- src/api/fetchTest.ts
+++ src/api/fetchTest.ts
@@ -1,16 +1,9 @@
 import{ useHttp } from '../plugins/http';
 
-const [api] = useHttp('/api');
-
-// export function fetchMockTest(): Promise<any> {
-//     return api.get('/test' /* path: /api/test */, { params: { t: Date.now().valueOf() } }).then((r: any) => {
-//         console.log(`获取到mock数据：${JSON.stringify(r)}`);
-//         return r;
-//     });
-// }
+const [api] = useHttp('/mock');
 
 export function fetchMockTest(): Promise<any> {
-    return api.get('/v1/info/app/download', { params: {appType: 2} }).then((r: any) => {
+    return api.get('/test', { params: { t: Date.now().valueOf() } }).then((r: any) => {
         console.log(`获取到mock数据：${JSON.stringify(r)}`);
         return r;
     });

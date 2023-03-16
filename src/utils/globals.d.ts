c629d279994c734c7ad816d9b20be672157e5a02
Abner <0x23fd5097@gmail.com>
2023-03-16 17:44:18 +0800

    chore: 类型修正

--- src/utils/globals.d.ts
+++ src/utils/globals.d.ts
@@ -35,7 +35,7 @@ declare global { // 声明进入全局命名空间的类型，或者增加全局
      * ReturnType的Promise版本
      * example: PromiseReturnType<(...rest: any[]) => Promise<string>> -> string
      */
-    type PromiseReturnType<T extends (...rest: any[]) => any> = ReturnType<T> extends Promise<infer R> ? R : ReturnType<T>;
+    type PromiseReturnType<T extends (...rest: any[]) => any> = [ReturnType<T>] extends [Promise<infer R>] ? R : ReturnType<T>;
 
     /**
      * 将异步函数返回的单个对象转换为数组的子项
@@ -49,7 +49,7 @@ declare global { // 声明进入全局命名空间的类型，或者增加全局
      * example: PromiseReturnArrayInner<(...rest: any[])=> Promise<string[]>> -> string
      * example: PromiseReturnArrayInner<(...rest: any[])=> Promise<{a: string[]}>, 'a'> -> string
      */
-    type PromiseReturnArrayInner<T extends (...rest: any[]) => any, U extends ObjectPropertyKey<PromiseReturnType<T> extends Array<any> ? ArrayInnerType<PromiseReturnType<T>> : PromiseReturnType<T>> = never> = [U] extends [never] ? ArrayInnerType<PromiseReturnType<T>> : ArrayInnerType<ObjectPropertyValue<PromiseReturnType<T> extends Array<any> ? ArrayInnerType<PromiseReturnType<T>> : PromiseReturnType<T>, U>>
+    type PromiseReturnArrayInner<T extends (...rest: any[]) => any, U extends ObjectPropertyKey<[PromiseReturnType<T>] extends [Array<any>] ? ArrayInnerType<PromiseReturnType<T>> : PromiseReturnType<T>> = never> = [U] extends [never] ? ArrayInnerType<PromiseReturnType<T>> : ArrayInnerType<[ObjectPropertyValue<[PromiseReturnType<T>] extends [Array<any>] ? ArrayInnerType<PromiseReturnType<T>> : PromiseReturnType<T>, U>]>
 
     /**
      * 将联合类型转成交叉类型
@@ -70,11 +70,11 @@ declare global { // 声明进入全局命名空间的类型，或者增加全局
     type PrettifyUnion<T> = Prettify<UnionToIntersection<T>>
 
     /**
-     * 将数组转换为对象（数组子项的特定 key 转换为对象的key）
+     * 将数组转换为对象（数组子项的特定 value 转换为对象的 key 和 value）
      * example: ArrayInnerPair<{ a: string, b: number }[]> => { [key in string | number]: string | number }
      * example: ArrayInnerPair<{ a: string, b: number }[], 'a', 'b'> => { [a in string]: number }
      */
-    type ArrayInnerPair<T extends Array<any>, U extends ObjectPropertyKey<T[number]> = ObjectPropertyKey<T[number]>, P extends ObjectPropertyKey<T[number]> = ObjectPropertyKey<T[number]>> = Record<ObjectPropertyValue<T[number], U>, ObjectPropertyValue<T[number], P>> 
+    type ArrayInnerPair<T extends Array<any>, U extends ObjectPropertyKey<T[number]> = ObjectPropertyKey<T[number]>, P extends ObjectPropertyKey<T[number]> = ObjectPropertyKey<T[number]>> = Record<[ObjectPropertyValue<T[number], U>] extends [PropertyKey] ? ObjectPropertyValue<T[number], U> : never, ObjectPropertyValue<T[number], P>> 
 
     /**
      * 提取函数的参数类型，Parameters 的单个参数版本
@@ -95,3 +95,4 @@ declare global { // 声明进入全局命名空间的类型，或者增加全局
      * example: ExcludeOuter<Promise<T>> => T
      */
     // type ExcludeOuter<T> = T extends Array<infer X> ? X : never
+}

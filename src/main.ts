5100a6c2876e14e506dde16a805c4f10a80d6dc7
Abner <0x23fd5097@gmail.com>
2023-08-07 22:41:45 +0800

    feat: 新增 polyfill.io

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,6 @@ import { createApp } from 'vue';
 import moment from 'dayjs';
 import { createPinia } from 'pinia'; /// doc: https://pinia.esm.dev
 import dayjsDuration from 'dayjs/plugin/duration';
-// Android8.1.0 Webview 需要主动填充
-import 'intl-pluralrules';
-// Android8.1.0 Webview 无法使用 Promise.finally，需要主动填充
-import 'finally-polyfill';
-// Android8.1.0 Webview 无法使用 Abortcontroller，需要主动填充
-import 'abortcontroller-polyfill/dist/abortcontroller-polyfill-only';
 
 import router from './setup/router-setup';
 import i18n from './setup/i18n-setup';
